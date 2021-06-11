7876da18dded60052c83b6a4b065d3388ed5cbf7
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2021-06-11 10:05:19 +0545

    updates

--- R/setup.R
+++ R/setup.R
@@ -47,7 +47,7 @@ install_pdf <- \() {
 
 run_jobs <- \(){
 
-path <- choose.files()
+path <- rstudioapi::selectFile()
 
 rstudioapi::jobRunScript(path, encoding = "utf-8", workingDir = path,
              importEnv = FALSE, exportEnv = "")
