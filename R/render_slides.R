43e1f2a6da0cda8cdd4a5dde27a124b39b5d99de
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2021-07-24 11:20:47 +0545

    added

---
+++ R/render_slides.R
@@ -0,0 +1,33 @@
+
+copy_folder <- function(source, dest, incl_dirs) {
+
+  old <- dir(path = here::here(source), pattern = incl_dirs, full.names = T)
+
+  new <- gsub(source, dest, old)
+
+  purrr::map2(.x = here::here(old), .y = here::here(new), R.utils::copyDirectory)
+
+}
+
+
+# copy_folder("docs", "slides2", "libs|imgs|figures")
+
+copy_file <- function(source, dest, pattern) {
+  files <- list.files(path = source, pattern = pattern, full.names = T)
+
+  file.copy(files, dest)
+}
+
+
+render_slides <- function(name = ".Rmd", docs = "docs",
+                          slides = "slides", ext = ".html|.css",
+                          incl_dirs = "libs|imgs|figures") {
+  files <- list.files(path = docs, pattern = name, full.names = T)
+
+  purrr::map(here::here(files), rmarkdown::render)
+
+  copy_file(docs, slides, ext)
+
+  copy_folder(docs, slides, incl_dirs)
+}
+
